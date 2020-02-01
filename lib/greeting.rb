27f422a18aa26abeae8776b48b65d47efc633288
Robert Aguilar <aguilar.r@live.com>
2020-02-01 06:09:01 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,6 @@
  # code the #greeting method here!
- greeting = "Yo"
+
  def greeting(name)
    puts "Hello #{name}. It's nice to meet you."
  end
  greeting(name)
-                                                                                                            
